9b5a07d7f7d40e686ccd7c8fa891457c64e1cc59
lxl66566 <lxl66566@gmail.com>
2025-11-02 21:14:33 +0800

    fix: arch compile
    Signed-off-by: lxl66566 <lxl66566@gmail.com>

--- xmake.lua
+++ xmake.lua
@@ -1,23 +1,28 @@
 SPEEDUP = 2.0
+if is_arch("x64") then
+	ARCH = "x64"
+else
+	ARCH = "x86"
+end
 
 set_encodings("utf-8")
 set_policy("build.warning", true)
 set_languages("c++20")
 set_optimize("fastest")
+if is_arch("x64") then
+	add_requires("soundtouch", { arch = ARCH })
+else
+	add_requires("soundtouch", { arch = ARCH })
+end
+-- add_requires("soundtouch", { configs = { shared = true } })
 
-target("mmdevapi_x64")
+target("mmdevapi")
 set_kind("shared")
-set_arch("x64")
-set_filename("MMDevAPI-win64-" .. SPEEDUP .. ".dll")
-add_files("MMDevAPI/x64/mmdevapi.cpp", "MMDevAPI/x64/mmdevapi.def", "MMDevAPI/x64/mmdevapi_asm.asm")
-add_links("ole32", "user32")
-add_defines("SPEEDUP=" .. SPEEDUP)
-
-target("mmdevapi_x86")
-set_kind("shared")
-set_arch("x86")
-set_filename("MMDevAPI-win32-" .. SPEEDUP .. ".dll")
-add_files("MMDevAPI/x86/mmdevapi.cpp", "MMDevAPI/x86/mmdevapi.def")
+set_filename("MMDevAPI-" .. ARCH .. "-" .. SPEEDUP .. ".dll")
+add_files("MMDevAPI/" .. ARCH .. "/mmdevapi.cpp", "MMDevAPI/" .. ARCH .. "/mmdevapi.def")
+if is_arch("x64") then
+	add_files("MMDevAPI/" .. ARCH .. "/mmdevapi_asm.asm")
+end
 add_links("ole32", "user32")
 add_defines("SPEEDUP=" .. SPEEDUP)
 
